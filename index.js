bc971ad3f9fb0ad505b41e7e5f57d27b5bb1d2de
Vaibhav <vaibhav@bluesapling.com>
2020-09-26 18:24:10 +0530

    Separate blog model and blogs controller from app

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const http = require('http')
 const express = require('express')
-const app = express()
+const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs');
 
 const password = process.argv[2];
 const mongoUrl = `mongodb+srv://fso_3c:${password}@cluster0.ih115.mongodb.net/bloglist?retryWrites=true&w=majority`
@@ -25,23 +17,7 @@ mongoose.connect(mongoUrl, {
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003
 app.listen(PORT, () => {
