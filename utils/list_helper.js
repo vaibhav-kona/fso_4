a9d78c9382c0adba9990b6ca5e1b6114e284c682
Vaibhav <vaibhav@bluesapling.com>
2020-09-29 18:52:53 +0530

    Finish 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce(getCumulativeLikeCount, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const getFavoriteBlog = (currentFavoriteBlog, currentBlog) => {
+    if (currentFavoriteBlog) {
+      const currentFavoriteBlogLikes = currentFavoriteBlog.likes;
+      const currentBlogLikes = currentBlog.likes;
+      const currentBlogHasMoreLikes = currentBlogLikes > currentFavoriteBlogLikes;
+      return currentBlogHasMoreLikes ? currentBlog : currentFavoriteBlog;
+    }
+    return currentBlog;
+  };
+  return blogs.reduce(getFavoriteBlog, null);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
