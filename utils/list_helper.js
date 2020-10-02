938f94f5a4c45be318dea20144f1ef1e89f023ad
Vaibhav <vaibhav@bluesapling.com>
2020-10-02 17:07:38 +0530

    Finish 4.6, 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -18,8 +18,55 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(getFavoriteBlog, null);
 };
 
+const mostBlogs = (blogs) => {
+  const blogCountForAuthor = {};
+  blogs.forEach((blog) => {
+    const currentBlogCount = blogCountForAuthor[blog.author];
+    const updatedBlogCount = currentBlogCount ? currentBlogCount + 1 : 1;
+    blogCountForAuthor[blog.author] = updatedBlogCount;
+  });
+
+  const mostBlogsData = {
+    author: '',
+    blogs: 0,
+  };
+  // Get one of the authors with most blogs
+  Object.keys(blogCountForAuthor).forEach((author) => {
+    if (blogCountForAuthor[author] > mostBlogsData.blogs) {
+      mostBlogsData.author = author;
+      mostBlogsData.blogs = blogCountForAuthor[author];
+    }
+  });
+
+  return mostBlogsData;
+};
+
+const mostLikes = (blogs) => {
+  const blogLikesForAuthor = {};
+  blogs.forEach((blog) => {
+    const currentBlogCount = blogLikesForAuthor[blog.author];
+    const updatedBlogCount = currentBlogCount ? currentBlogCount + blog.likes : blog.likes;
+    blogLikesForAuthor[blog.author] = updatedBlogCount;
+  });
+
+  const mostLikedAuthor = {
+    author: '',
+    likes: 0,
+  };
+  Object.keys(blogLikesForAuthor).forEach((author) => {
+    if (blogLikesForAuthor[author] > mostLikedAuthor.likes) {
+      mostLikedAuthor.author = author;
+      mostLikedAuthor.likes = blogLikesForAuthor[author];
+    }
+  });
+
+  return mostLikedAuthor;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
