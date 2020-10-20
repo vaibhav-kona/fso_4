2dd0509f73bc1eda0e2569a3c53d9a8c70b6521d
Vaibhav <vkona8@gmail.com>
2020-10-20 10:56:07 +0530

    Finish upto 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -20,6 +20,15 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
+blogsRouter.put('/:blogId', async (req, res) => {
+  const blog = await Blog.findById(req.params.blogId);
+  console.log('blog : ', blog);
+  blog.likes = req.body.likes || 0;
+  const updatedBlog = await blog.update();
+  console.log('updatedBlog : ', updatedBlog);
+  res.status(200).json(updatedBlog);
+});
+
 blogsRouter.delete('/:blogId', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.blogId).then((blogs) => {
     response.json(blogs);
