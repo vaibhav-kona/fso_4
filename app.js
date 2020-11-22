c037c4bce61d0afccae98860c5dd4de04bc9166c
Vaibhav <vkona8@gmail.com>
2020-11-22 10:18:28 +0530

    Add testing router

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/test');
+  app.use('/api/testing', testingRouter);
+}
 
 // Middleware
 app.use(middleware.unknownEndpoint);
